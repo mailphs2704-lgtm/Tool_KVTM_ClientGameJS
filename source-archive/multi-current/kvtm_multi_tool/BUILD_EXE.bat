876f2521f398ce63f67c9f70352f40c7973bafd2
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-29 20:29:02 +0700

    build(multi): use reliable onedir executable layout

--- source-archive/multi-current/kvtm_multi_tool/BUILD_EXE.bat
+++ source-archive/multi-current/kvtm_multi_tool/BUILD_EXE.bat
@@ -2,8 +2,9 @@
 setlocal
 cd /d "%~dp0"
 py -3 -m pip install --upgrade pyinstaller
-py -3 -m PyInstaller --noconfirm --clean --onefile --windowed --name "KVTM-Multi" kvtm_multi.py
+py -3 -m PyInstaller --noconfirm --clean --onedir --windowed --name "KVTM-Multi" kvtm_multi.py
 echo.
-echo File EXE: dist\KVTM-Multi.exe
+echo Thu muc chay: dist\KVTM-Multi\
+echo File EXE: dist\KVTM-Multi\KVTM-Multi.exe
 pause
 endlocal
