7f8ad5f1e8220149be11945e19a3cb1ac72c9d0f
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-31 17:58:39 +0700

    Export Dọn quầy scheduler helpers

--- components/clientjs-auto/workflows/clear_stall/__init__.py
+++ components/clientjs-auto/workflows/clear_stall/__init__.py
@@ -4,5 +4,24 @@ This package is independent from AUTO PRO FarmAutomation function IDs.
 """
 
 from .config import ClearStallJob, checkpoint, normalize_job, schedule_next
+from .scheduler import (
+    DueClearStallJob,
+    any_profile_running,
+    collect_due_jobs,
+    mark_completed,
+    mark_failed,
+    mark_started,
+)
 
-__all__ = ["ClearStallJob", "checkpoint", "normalize_job", "schedule_next"]
+__all__ = [
+    "ClearStallJob",
+    "DueClearStallJob",
+    "any_profile_running",
+    "checkpoint",
+    "collect_due_jobs",
+    "mark_completed",
+    "mark_failed",
+    "mark_started",
+    "normalize_job",
+    "schedule_next",
+]
