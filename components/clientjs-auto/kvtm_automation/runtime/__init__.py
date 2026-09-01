ed8b1f9296981f3cb8cb13c47f4cec70e41bf983
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-01 07:14:50 +0700

    Add clean automation runtime package

---
+++ components/clientjs-auto/kvtm_automation/runtime/__init__.py
@@ -0,0 +1,6 @@
+from .assets import AssetLibrary
+from .driver import ClientJSDriverFactory
+from .vision import Match, VisionEngine
+from .wait import Waiter
+
+__all__ = ["AssetLibrary", "ClientJSDriverFactory", "Match", "VisionEngine", "Waiter"]
