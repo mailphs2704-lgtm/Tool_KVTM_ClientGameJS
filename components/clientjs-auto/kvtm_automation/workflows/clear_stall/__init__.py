0ca1aefb7b68c59f7850bc70613ce249e84ac2af
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-01 07:28:36 +0700

    Add clean Dọn quầy workflow package

---
+++ components/clientjs-auto/kvtm_automation/workflows/clear_stall/__init__.py
@@ -0,0 +1,5 @@
+from .config import ClearStallRequest
+from .result import ClearStallResult
+from .workflow import ClearStallWorkflow
+
+__all__ = ["ClearStallRequest", "ClearStallResult", "ClearStallWorkflow"]
