c3e0d9dd3304ecf675cfd5d54599b9509f9b30da
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-01 07:18:23 +0700

    Add reusable automation action modules

---
+++ components/clientjs-auto/kvtm_automation/actions/__init__.py
@@ -0,0 +1,13 @@
+from .inventory import InventoryActions
+from .navigation import NavigationActions
+from .popup import PopupActions
+from .selling import SellingActions
+from .stall import StallActions
+
+__all__ = [
+    "InventoryActions",
+    "NavigationActions",
+    "PopupActions",
+    "SellingActions",
+    "StallActions",
+]
