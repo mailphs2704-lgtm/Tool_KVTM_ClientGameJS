030db8a82205bbd96b135db4271d60b7bcb0fbb6
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-05 22:31:21 +0700

    feat(main): export VP recognition workflow

---
+++ components/clientjs-auto/kvtm_automation/workflows/vp_recognition/__init__.py
@@ -0,0 +1,6 @@
+from .workflow import VpRecognitionProbeResult, VpRecognitionProbeWorkflow
+
+__all__ = ["VpRecognitionProbeResult", "VpRecognitionProbeWorkflow"]
+FILE_FUNCTIONS = (
+    "Xuất API workflow kiểm tra nhận diện VP",
+)
