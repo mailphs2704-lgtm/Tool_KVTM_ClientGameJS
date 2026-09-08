73dd700f54cc7cc27da0991d85ff6564af54f9ab
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-08 23:59:42 +0700

    Install Function loop delay runtime

--- components/clientjs-auto/kvtm_automation/workflows/auto_builder/__init__.py
+++ components/clientjs-auto/kvtm_automation/workflows/auto_builder/__init__.py
@@ -1,5 +1,8 @@
 from .catalog import FUNCTION_SPECS, FunctionSpec, get_function_spec
 from .runner import AutoBuilderResult, AutoBuilderRunner, validate_plan
+from .loop_delay_patch import install_function_loop_delay
+
+install_function_loop_delay(AutoBuilderRunner)
 
 __all__ = [
     "FUNCTION_SPECS",
