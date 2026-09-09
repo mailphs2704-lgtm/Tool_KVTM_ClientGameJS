f907ba36a015ebf90d0bde8ce0511a9ddfad3a86
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-09 19:03:23 +0700

    Add reusable production recipe package

---
+++ components/clientjs-auto/kvtm_automation/recipes/__init__.py
@@ -0,0 +1,14 @@
+from .apple_juice import AppleJuiceRecipe, AppleJuiceRecipeResult
+from .book import RecipeBook
+from .dried_apple import DriedAppleRecipe, DriedAppleRecipeResult
+from .yellow_fabric import YellowFabricRecipe, YellowFabricRecipeResult
+
+__all__ = [
+    "AppleJuiceRecipe",
+    "AppleJuiceRecipeResult",
+    "DriedAppleRecipe",
+    "DriedAppleRecipeResult",
+    "RecipeBook",
+    "YellowFabricRecipe",
+    "YellowFabricRecipeResult",
+]
