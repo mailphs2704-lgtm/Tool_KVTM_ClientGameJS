fef33fdf2e8b27be98f6ab19804e8e2946692c97
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-10 07:55:27 +0700

    Wire material-aware production actions

--- components/clientjs-auto/kvtm_automation/actions/__init__.py
+++ components/clientjs-auto/kvtm_automation/actions/__init__.py
@@ -1,20 +1,25 @@
-from .apple_juice_production import AppleJuiceProductionActions
 from .apple_supply import AppleSupplyActions
 from .buying import BuyingActions
-from .cotton_planting import CottonPlantingActions
 from .floor_navigation import FloorMoveResult, FloorNavigationActions
 from .function_one_navigation import FunctionOneNavigationActions, NavigationEvidence
 from .function_one_pass_three_navigation import FunctionOnePassThreeNavigationActions
 from .inventory import InventoryActions
 from .item_recognition import AutoVpRecognitionActions, AutoVpSpec, VpRecognition
-from .machine_repair import MachineRepairActions, MachineRepairHandoff
+from .material_shortage_production import (
+    MaterialAwareAppleJuiceProductionActions as AppleJuiceProductionActions,
+    MaterialAwareCottonPlantingActions as CottonPlantingActions,
+    MaterialAwareMachineRepairActions as MachineRepairActions,
+    MaterialAwareProductionActions as ProductionActions,
+    MaterialAwareProductionResult,
+    MaterialAwareYellowFabricProductionActions as YellowFabricProductionActions,
+)
+from .machine_repair import MachineRepairHandoff
 from .navigation import NavigationActions
 from .popup import PopupActions
 from .planting import PlantingActions
-from .production import ProductionActions, ProductionResult
+from .production import ProductionResult
 from .selling import SellingActions
 from .stall import StallActions
-from .yellow_fabric_production import YellowFabricProductionActions
 
 __all__ = [
     "AppleJuiceProductionActions",
@@ -32,6 +37,7 @@ __all__ = [
     "VpRecognition",
     "MachineRepairActions",
     "MachineRepairHandoff",
+    "MaterialAwareProductionResult",
     "NavigationActions",
     "PopupActions",
     "PlantingActions",
