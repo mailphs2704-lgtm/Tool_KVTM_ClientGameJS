e6afb2ffc7c641e872a887a6f6bbc5c5e70c1ee5
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-09-10 07:56:31 +0700

    Wire material-aware recovery manager

--- components/clientjs-auto/kvtm_automation/recovery/__init__.py
+++ components/clientjs-auto/kvtm_automation/recovery/__init__.py
@@ -1,7 +1,10 @@
 from .events import RecoveryEvent, RecoveryEventKind
-from .manager import RecoveryEventHandler, RecoveryManager
+from .manager import RecoveryEventHandler
+from .material_shortage import (
+    MaterialAwareProductionRecovery as ProductionRecovery,
+    MaterialAwareRecoveryManager as RecoveryManager,
+)
 from .navigation import NavigationRecovery, RouteHandler
-from .production import ProductionRecovery
 
 __all__ = [
     "RecoveryEvent",
